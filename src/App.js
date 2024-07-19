832bb5fe58aefd1da7e29c5acb9fbedd9b96668c
archana-kondoju2 <archanakondoju2@email.com>
2024-07-19 23:32:38 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Board from "./components/Board";
+import ToolBar from "./components/ToolBar";
+import Toolbox from "./components/Toolbox";
+import BoardProvider from "./store/BoardProvider";
+import ToolboxProvider from './store/ToolboxProvider';
+function App() {
+  return (
+    <BoardProvider>
+      <ToolboxProvider>
+        <ToolBar/>
+        <Board/>
+        <Toolbox/>
+      </ToolboxProvider> 
+    </BoardProvider>
+  );
+}
+
+export default App;
